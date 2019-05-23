cb57730fe3ed4c7808fa50f2ef143a2f9984a31a
michael dimmitt <michaelgdimmitt@gmail.com>
2019-05-22 21:46:02 -0400

    Blue shirt svg added.

--- src/Svg/index.js
+++ src/Svg/index.js
@@ -1,3 +1,4 @@
 export { default as BaldGuy } from './BaldGuy';
+export { default as BaldGuyB } from './BaldGuyB';
 export { default as LunchLadyHairNet } from './LunchLadyHairNet';
 export { default as NurseHat } from './NurseHat';
