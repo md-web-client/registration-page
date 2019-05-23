0d933f2aac5da78cc3c093142ac0f8299e7f1eb2
michael dimmitt <michaelgdimmitt@gmail.com>
2019-05-22 23:00:16 -0400

    Convert use-state hook to setState in component.

--- src/components/Fields/Fields.js
+++ src/components/Fields/Fields.js
@@ -1,47 +1,64 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, Component } from 'react';
 
-const Input = ({index, name, placeholder}) => {
-  const [ fieldText, setFieldText ] = useState("")
-  function handleChange(event) {
-    let { value } = event.currentTarget;
-    setFieldText(value.trim())
-  }
-  const capName = name.charAt(0).toUpperCase() + name.slice(1)
+const Input = ({index, name, placeholder, fields, handleChange}) => {
+  const capName = name.charAt(0).toUpperCase() + name.slice(1);
 
   return (
-  <div className={"inputGroup inputGroup"+index}>
-    <label id={'login'+ capName +'Label'} htmlFor={'login'+ capName}>
-      {capName}
-      {name === 'password' && (
-      <span id="showPasswordToggle" htmlFor="showPasswordCheck">
-        <input id="showPasswordCheck" type="checkbox" />
-      </span> )}
-    </label>
-    {
-      fieldText === '' && placeholder ?
-      <input onChange={handleChange} value="&nbsp;" type={name} name={name} id={'login'+capName} className={name} maxLength={256} />
-      : <input onChange={handleChange} value={ fieldText } type={placeholder ? "text" : name } name={name} id={'login'+capName} className={name} maxLength={256} />
-    }
-    { placeholder ?
-        <p className={"helper helper"+index}>{placeholder}</p>
-        : undefined
-    }
-  </div>
-  )
+    <div className={"inputGroup inputGroup"+index}>
+      <label id={'login'+ capName +'Label'} htmlFor={'login'+ capName}>
+        {capName}
+        {name === 'password' && (
+        <span id="showPasswordToggle" htmlFor="showPasswordCheck">
+          <input id="showPasswordCheck" type="checkbox" />
+        </span> )}
+      </label>
+      {
+        fields[name] === '' && placeholder ?
+        <input onChange={handleChange} value="&nbsp;" type={name} name={name} id={'login'+capName} className={name} maxLength={256} />
+        : <input onChange={handleChange} value={ fields[name] } type={placeholder ? "text" : name } name={name} id={'login'+capName} className={name} maxLength={256} />
+      }
+      { placeholder ?
+          <p className={"helper helper"+index}>{placeholder}</p>
+          : undefined
+      }
+    </div>
+  );
 }
 // index, email, Email, email@domain.com
 
-function Fields() {
-  return (
-    <Fragment>
-      <Input index={1} name={"email"} placeholder={"email@domain.com"}/>
-      <Input index={2} name={"password"} placholder={""}/>
-      <div className="inputGroup inputGroup3">
-        <button style={{ height: '100px' }} id="login">Log in</button>
-      </div>
+class Fields extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      fields: {
+        email: "",
+        password: ""
+      }
+    };
+    this.handleChange = this.handleChange.bind(this);
+  };
 
-    </Fragment>
-  );
+  handleChange(event) {
+    let { name, value } = event.currentTarget;
+    let { fields } = this.state;
+    fields[name] = value.trim();
+    this.setState({fields})
+  };
+
+  render(){
+    const { fields } = this.state;
+    console.log(fields);
+
+    return (
+      <Fragment>
+        <Input index={1} fields={fields} handleChange={this.handleChange} name={"email"} placeholder={"email@domain.com"} />
+        <Input index={2} fields={fields} handleChange={this.handleChange} name={"password"} placholder={""} />
+        <div className="inputGroup inputGroup3">
+          <button style={{ height: '100px' }} id="login" >Log in</button>
+        </div>
+      </Fragment>
+    );
+  };
 }
 
 export default Fields;
