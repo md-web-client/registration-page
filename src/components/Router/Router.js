d8dfd87a94c7bc899ac05a37aa5e85c8787178a2
michael dimmitt <michaelgdimmitt@gmail.com>
2019-05-24 17:03:37 -0400

    redirect root to register

--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { MaleNurseLogin, Users, /* LunchLadyLogin, BaldGuyLogin */ } from '../Pages';
 import { navigate } from '../../helpers'
 
 export default class Router extends Component {
-  
+
   render(){
   return (
     <BrowserRouter>
+      <Route exact path="/" component={()=>(
+        <Redirect to="/register"/>
+      )}/>
       <Switch>
-        <Route exact path={['/', '/register']} 
+        <Route exact path={['/Register', '/register']}
           component={routeProps => (
             <MaleNurseLogin {...routeProps} />
           )}
         />
-        <Route exact path="/users" 
+        <Route exact path={['/Users', '/users']}
           component={routeProps => (
             <Users {...routeProps} />
           )}
