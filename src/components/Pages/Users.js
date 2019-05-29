31ead47ed4cde597101cd83afc5df92c2bb380fc
michael dimmitt <michaelgdimmitt@gmail.com>
2019-05-29 09:37:45 -0400

    Hook up ec2 instance to database api.

--- src/components/Pages/Users.js
+++ src/components/Pages/Users.js
@@ -1,41 +1,53 @@
 import React, { Component } from 'react';
 
 const getUsers = () => {
-  return fetch('http://localhost:6789/postgres/users')
+  return fetch('http://3.19.120.4/mongo/users/')
     .then(function(response) { return response.json(); })
     .then(function(response) { return (console.log('Get', 'fetch', response), response) })
-    .catch(function (error)   { console.log({error}) })
 }
 
-const User = ({user}) => (
+const User = ({user}) => {
+  console.log({user})
+  return(
   <div style={{display: 'flex', justifyContent: 'space-evenly'}}>
+    <br/>
     <br/>
     {
       Object.values(user).map(item => <span style={{marginLeft: '15px'}}>{item}</span>)
     }
+  </div>
+)}
+const ErrorMessage = ({message}) => (
+  <div>
+    {message}
   </div>
 )
 
 export default class Users extends Component {
   constructor(){
     super()
-    this.state = {userInfo:[]}
+    this.state = {userInfo:[], message: ''}
   }
   componentWillMount(){
     getUsers()
     .then(result => this.setState({userInfo: result}))
+    .catch(error => this.setState({ message: 'check internet or api connection, thanks' }))
   }
   render(){
-    const { userInfo } = this.state
-    console.log({userInfo})
+    const { userInfo, message } = this.state
+    console.log({userInfo, message})
     return (
-      <div>
+      <span>
         Users
+        <br/>
+        <br/>
         {
-          userInfo && userInfo.map(user =>
+          userInfo.length > 0 ? userInfo.map(user =>
             <User user={user}/>
           )
+          : message ? <ErrorMessage message={message}/>
+          : ''
         }
-      </div>
+      </span>
     )};
 }
